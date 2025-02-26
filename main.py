58c1caef25eab695b9c50da78ab174262bac6ce6
Fernando <fernandopereira-3@hotmail.com>
2025-02-26 12:45:17 -0300

    css 90%

--- main.py
+++ main.py
@@ -104,7 +104,7 @@ def completa():
 
 @app.route('/')
 def index():
-    return render_template('ex.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=80)
