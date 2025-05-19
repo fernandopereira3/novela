026247cb950752c896f5b1c6de152deaf02bab0b
Fernando <fernandopereira-3@hotmail.com>
2025-05-19 13:51:56 -0300

    Update do GIT automatico / 19-05-25 as 13:51

--- update.py
+++ update.py
@@ -1,16 +1,17 @@
 import datetime
 import subprocess
 
-date = datetime.datetime.now().strftime('%d-%m-%y -> %H:%M')
+date = datetime.datetime.now().strftime('%d-%m-%y as %H:%M')
 
 print('Processo iniciado...')
 subprocess.run(['clear'])
 print('Realizando checagem de erros...')
 subprocess.run(['ruff', 'check', '.'])
 subprocess.run(['clear'])
+commit = input('Escreva o commit: ')
 print('Realizando commit...')
 subprocess.run(['git', 'add', '.'])
-subprocess.run(['git', 'commit', '-m', date])
+subprocess.run(['git', 'commit', '-m', f'{commit} / {date}'])
 subprocess.run(['git', 'push', 'origin', 'main'])
 subprocess.run(['clear'])
 print('Processo finalizado com sucesso!')
